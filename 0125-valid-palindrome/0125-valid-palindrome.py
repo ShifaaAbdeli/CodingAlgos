7e357931948178b55fa8be5c3762a0f2c8ed0232
AbdeliB <34876910+ShifaaAbdeli@users.noreply.github.com>
2024-01-30 12:56:07 -0800

    Time: 57 ms (24.33%), Space: 17 MB (89.94%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,18 +1,32 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-
-        s = ''.join(c for c in s if c.isalnum())
-        s = s.lower()
-        return s == s[::-1]
-        # Removing non-alphanumeric characters
-        #s = ''.join(filter(str.isalnum, s))
-        # Convert the string to lower cases
+        # First Approach:
+        # B.Force:
+        #s = ''.join(c for c in s if c.isalnum())
         #s = s.lower()
-        # Use two pointers approch Left and Right
-        #for i in range(len(s) - 1):
-        #    if s[i] != s[len(s) - 1 -i]:
-        #        return False
-        #return True
         #return s == s[::-1]
 
         
+        # Second approach:
+        # Removing non-alphanumeric characters. 
+        # and compare the charactaires
+        l, r = 0, len(s) - 1
+
+        # Alpha Numiric function:
+        def alphaNum(c):
+            return (ord('a') <= ord(c) <= ord('z') or
+                    ord('A') <= ord(c) <= ord("Z") or
+                    ord('0') <= ord(c) <= ord('9'))
+                
+        while l < r:
+            while l < r and not alphaNum(s[l]):
+                l += 1
+            while l < r and not alphaNum(s[r]):
+                r -= 1
+                
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+            
+        return True
