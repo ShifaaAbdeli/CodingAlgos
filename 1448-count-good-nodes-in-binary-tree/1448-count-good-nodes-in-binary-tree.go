97c72e3daf5fbf697a987282fc82a870b027ab10
AbdeliB <34876910+ShifaaAbdeli@users.noreply.github.com>
2024-02-02 11:24:39 -0800

    Time: 74 ms (78.97%), Space: 11.3 MB (63.65%) - LeetHub

---
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.go
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func dfsHelper(node *TreeNode, maxVal int) int {
+    res := 0
+    if node == nil {
+        return 0
+    }
+    if node.Val >= maxVal {
+        res = 1
+    } else {
+        res = 0
+    }
+    maxVal = max(maxVal, node.Val)
+    res += dfsHelper(node.Left, maxVal)
+    res += dfsHelper(node.Right, maxVal)
+    
+    return res
+}
+func goodNodes(root *TreeNode) int {
+    return dfsHelper(root, root.Val)
+}
