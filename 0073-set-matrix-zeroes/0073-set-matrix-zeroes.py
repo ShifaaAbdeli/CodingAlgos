2eb9f79d45484d55cc290dc58048f4c198267349
AbdeliB <34876910+ShifaaAbdeli@users.noreply.github.com>
2024-02-07 20:08:13 -0800

    Time: 104 ms (64.67%), Space: 17.5 MB (77.47%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -0,0 +1,77 @@
+"""
+        B.F.: 
+             [[1,1,1],
+              [1,0,1],
+              [1,1,1]]
+              
+        for i in range(ROW):
+          for j in range(COL):
+              if matrix[i][j] == 0:
+                 k = 0
+                 l = 0
+                 while k < COL:
+                    matrix[i][k] = 0
+                 while l < ROW:
+                    matrix[l][j] = 0
+        Time complexity: O(mxnxmxn). Space: O(1)
+        
+        
+        Improve time complexity:
+        ========================
+        - Loop on ROW and COL and if an element 
+          is null set its first element of the 
+          row to 0 and first elementb of colon to 0
+          
+        - Loop on ROW and COL and if fisrt elemnt of 
+          ROW or COL is 0 set all ROW to colon tto 0
+          
+          
+             [[1,1,1],
+              [1,0,1],
+              [1,1,1]]
+              
+             [[1,0,1],
+              [1,0,1],
+              [1,1,1]]
+"""
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        ROW = len(matrix)
+        COL = len(matrix[0])
+        rowZero = False
+        
+        for r in range(ROW):
+            for c in range(COL):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0
+                    if r > 0:
+                        matrix[r][0] = 0
+                    elif r == 0:
+                        rowZero = True
+                    
+        for r in range(1,ROW):
+            for c in range(1, COL):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
+                  
+        if matrix[0][0] == 0:
+            for r in range(ROW):
+                matrix[r][0] = 0
+                
+        if rowZero == True:
+            for c in range(COL):
+                matrix[0][c] = 0
+    """               
+        [[0,1,2,0],
+         [3,4,5,2],
+         [1,3,1,5]]
+        
+        [[0,1,2,0],
+         [0,0,0,2],
+         [0,0,0,5]]  
+    """
+                    
