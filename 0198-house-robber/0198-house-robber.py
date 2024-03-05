bea8f8aacf2f499c0adb7721601eb2ddceb63403
AbdeliB <34876910+ShifaaAbdeli@users.noreply.github.com>
2024-03-04 19:23:33 -0800

    Time: 43 ms (11.74%), Space: 16.6 MB (25.17%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,17 +1,20 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         #### DP approach ####
+        # [1   , 2   , 3,  1]
+        #  rob1, rob2, n, n+1, ...
         # 1- [1,2,3,1], need to start to rob a 
-        #       max money house 1 (rob1) or 2
+        #     house 1 (rob1) or house 2
         #       (rob2) from the nums above.
         # 2- Then next possible house to rob is
-        #   house 3 (house n) to avoid the alarm. 
+        #   house 3 (house n) to avoid the alarm.
+        
         # 3- To maximize the money, need to take
         #     the max of (rob1 + n, rob2) of the
-        #      two possibility above
-        # 4- We can return the max (we can
-        #     maintined max in rob2) and return it
-        #     at the end
+        #      two possibility above at (2)
+        #
+        # 4- next the rob1 became rob2 and rob2 became the max and return rob2
+        #     at the end of the loop on nums
         ###
         rob1, rob2 = 0, 0
         for n in nums:
