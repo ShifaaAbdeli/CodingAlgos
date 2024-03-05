7cbe56fe5c984c622c7f1985cd585bb35abfea44
AbdeliB <34876910+ShifaaAbdeli@users.noreply.github.com>
2024-03-04 19:46:39 -0800

    Time: 1 ms (79.37%), Space: 2 MB (68.82%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.go
@@ -0,0 +1,28 @@
+/*#### DP approach ####
+        # [1   , 2   , 3,  1]
+        #  rob1, rob2, n, n+1, ...
+        # 1- [1,2,3,1], need to start to rob a 
+        #     house 1 (rob1) or house 2
+        #       (rob2) from the nums above.
+        # 2- Then next possible house to rob is
+        #   house 3 (house n) to avoid the alarm.
+        
+        # 3- To maximize the money, need to take
+        #     the max of (rob1 + n, rob2) of the
+        #      two possibility above at (2)
+        #
+        # 4- next the rob1 became rob2 and rob2 became the max and return rob2
+        #     at the end of the loop on nums
+### */
+    
+func rob(nums []int) int {
+    rob1 := 0
+    rob2 := 0
+    
+    for n := 0; n < len(nums); n++ {
+        temp := max(rob1+ nums[n], rob2)
+        rob1 = rob2
+        rob2 = temp
+    }
+    return rob2
+}
