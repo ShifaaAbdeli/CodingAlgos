251aeef6ae3fce1aef66a70dbc8edc39f75b0da6
AbdeliB <34876910+ShifaaAbdeli@users.noreply.github.com>
2024-03-06 13:28:46 -0800

    Time: 8 ms (68.98%), Space: 5.2 MB (61.70%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.go
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.go
@@ -1,4 +1,4 @@
-"""
+/*
 [
   1->4->5,
   1->3->4,
@@ -21,45 +21,57 @@ Approach:
 - Time comlexity is n*log(k), since we keep dividing the lists by 2 logk times
   Space complexity is: O(k)
 
-"""
+*/
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if lists is None or len(lists) == 0:
-            return None
+func mergeTwoList(list1 *ListNode, list2 *ListNode) *ListNode {
+    
+    dummy := new(ListNode)
+    tail := dummy
+    
+    for (list1 != nil && list2 != nil) {
+        if list1.Val < list2.Val {
+            tail.Next = list1
+            list1 = list1.Next
+        } else {
+            tail.Next = list2
+            list2 = list2.Next
+        }
+        tail = tail.Next
+    }
+    
+    if list1 != nil {
+        tail.Next = list1
+    }
+    if list2 != nil {
+        tail.Next = list2
+    }
+    
+    return dummy.Next
+}
 
-        while len(lists) > 1:
-            mergeList = []
-            for i in range(0, len(lists), 2):
-                l1 = lists[i]
-                l2 = lists[i+1] if i+1 < len(lists) else None
-                mergeList.append(self.mergeTwoList(l1, l2))
-            lists = mergeList
-
-        return lists[0]
-
-    def mergeTwoList(self, l1, l2):
-        dummy = ListNode()
-        tail = dummy
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeKLists(lists []*ListNode) *ListNode {
+    if lists == nil || len(lists) == 0 {
+        return nil
+    }
+    
+    for len(lists) > 1 {
+        merge := []*ListNode{}
         
-        while l1 and l2:
-            if l1.val < l2.val:
-                tail.next = l1
-                l1 = l1.next
-            else:
-                tail.next = l2
-                l2 = l2.next
-            # tail.next already filled, move to the tail.next
-            tail = tail.next
-
-        if l1:
-            tail.next = l1
-        if l2:
-            tail.next = l2
-
-        return dummy.next
+        for i:= 0; i < len(lists); i += 2 {
+            if i+1 >= len(lists) {
+                merge = append(merge, lists[i])
+                continue
+            }           
+            merge = append(merge, mergeTwoList(lists[i], lists[i+1]))
+        }
+        lists = merge
+    }
+    return lists[0]
+}
