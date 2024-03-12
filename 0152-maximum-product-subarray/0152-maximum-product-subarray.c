38d21fccc0952831c5e6e5df48bf746d79df962d
AbdeliB <34876910+ShifaaAbdeli@users.noreply.github.com>
2024-03-12 13:06:50 -0700

    Time: 8 ms (14.81%), Space: 6.4 MB (54.50%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.c
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.c
@@ -1,76 +1,29 @@
-"""
- - nums = [2,3,-2,4]
-
- BF. :
- =====
- Multiplay the subarray element from a loop on the array and keep the max
-      largProd = 0
-      loop x in range(len(nums)):
-         loop y in range(x, len(nums) - x):
-            largProd = max(largProd, nums[x]*nums[y])
-      return largProd
-      
-      - Time Complexity: O(n^2), space: O(1)
-      
- Dynamic Programming:
- ===================
- - nums = [2,3,-2,4]
- 
-           [2]
-           [2,3] 
-           [2,3,-2]
-           [2,3,-2,4]
-           [3]
-           [3,-2]
-           [3,-2,4]
-           [-2]
-           [-2,4]
-           [4]
-         
-    curMin, curMax = 1, 1
-    n = nums[0] = 2; tempMax = curMax x n, 
-        curMax = 2x1, curMin = min(tempMax, n x curMin, n) = 2
-    n = nums[1] = 3; tempMax = curMax x n = 6, 
-        curMax = max(curMax x n, curMin x n, n) = 6
-        curMin = min(tempMax, curMin, n) = 2
- 
- Second Approach:
- ===============
-    pre = 1
-    surf = 1
-    maxSubArr = INT_MIN
-    for i in range(len(nums)):
-        if pre == 0:
-           pre = 1
-        if surf == 0:
-           surf = 1
-           
-        pre = pre * nums[i]
-        suff = suff * nums[n-1-i]
-        maxSubArr = max(maxSubArr, pre, suff)
-        
-    return maxSubArr
+int max(int x, int y) {
     
-"""
+    if (x > y) {
+        return x;
+    } else {
+        return y;
+    }
+}
 
-class Solution:
-    def maxProduct(self, nums: List[int]) -> int:
-        pref = 1
-        surff = 1
-        maxSubArr = max(nums)
-        ARR = len(nums)
+int maxProduct(int* nums, int numsSize) {
+    int prefx = 1;
+    int surffx = 1;
+    int maxSub = nums[0];
         
-        for i in range(ARR):
-            if pref == 0:
-                pref = 1
-            if surff == 0:
-                surff = 1
-                
-            pref = pref * nums[i]
-            surff = surff * nums[ARR-1-i]
-            maxSubArr = max(maxSubArr, pref, surff)
-            
-        return maxSubArr
+    for (int i = 0; i < numsSize; i++) {
+        if (prefx == 0) {
+            prefx = 1;
+        }
+        if (surffx == 0) {
+            surffx = 1;
+        }
         
-        
-        
+        prefx = prefx * nums[i];
+        surffx = surffx * nums[numsSize -1-i];
+        maxSub = max(maxSub, prefx);
+        maxSub = max(maxSub, surffx);
+    }
+    return maxSub;
+}
