483bf8295f3527887f226615d4467d950ccf72de
AbdeliB <34876910+ShifaaAbdeli@users.noreply.github.com>
2024-03-13 13:33:41 -0700

    Time: 32 ms (77.40%), Space: 16.6 MB (15.10%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -1,3 +1,30 @@
+"""
+ Input: root = [4,2,7,1,3,6,9]
+ tmp = 2
+ left = 7
+ right = tmp = 2
+ 4, 7, 2, 1,3,6,9
+ 
+ tmp = 7.6
+ left = 9
+ right = tmp = 6
+ 4,7,2,9,6,1,3
+ 
+ tmp = left = 2.1
+ left = 3
+ right = tmp = 1
+ 4,7,2,9,6,3,1
+ 
+ - tmp = r.left
+   r.left = r.right
+   r.right = tmp
+   invertTree(r.left)
+   invertTree(r.right)
+   
+  Time Complexity: O(n), Space: O(1)
+"""
+
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,26 +34,19 @@
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         
-        """ BFS Approach
-        if root is None:
+        if not root or root == []:
             return root
-
-        deq = deque([root])
-        while deq:
-            p = deq.pop()
-            p.left,p.right = p.right,p.left
-            if p.left:
-                deq.append(p.left)
-            if p.right:    
-                deq.append(p.right)
-        return root
-        """
-        # DFS Recursive approch
-        if root is None:
-           return root
-        root.left, root.right = root.right, root.left
-        if root.left is not None:
+        
+        head = root
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+        
+        if root.left:
             self.invertTree(root.left)
-        if root.right is not None:
+        if root.right:
             self.invertTree(root.right)
-        return root
+            
+        return head
+        
+        
