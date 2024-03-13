ddfe64c282802a6a4a6e14d2d0f23f3d0aa8ad3c
AbdeliB <34876910+ShifaaAbdeli@users.noreply.github.com>
2024-03-12 17:09:11 -0700

    Time: 3 ms (86.47%), Space: 16.2 MB (41.22%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,29 +1,31 @@
-int max(int x, int y) {
+class Solution {
+public:
     
-    if (x > y) {
-        return x;
-    } else {
-        return y;
-    }
-}
-
-int maxProduct(int* nums, int numsSize) {
-    int prefx = 1;
-    int surffx = 1;
-    int maxSub = nums[0];
-        
-    for (int i = 0; i < numsSize; i++) {
-        if (prefx == 0) {
-            prefx = 1;
+    int max (int x, int y) {
+        if (x > y) {
+            return x;
+        } else {
+            return y;
         }
-        if (surffx == 0) {
-            surffx = 1;
-        }
-        
-        prefx = prefx * nums[i];
-        surffx = surffx * nums[numsSize -1-i];
-        maxSub = max(maxSub, prefx);
-        maxSub = max(maxSub, surffx);
     }
-    return maxSub;
-}
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int prefx = 1;
+        int surffx = 1;
+        int maxSub = nums[0];
+        
+        for (int i = 0; i < n; i++) {
+            if (prefx == 0) {
+                prefx = 1;
+            }
+            if (surffx == 0) {
+                surffx = 1;
+            }
+            prefx = prefx * nums[i];
+            surffx = surffx * nums[n-1-i];
+            maxSub = max(maxSub, prefx);
+            maxSub = max(maxSub, surffx);
+        }
+        return maxSub;
+    }
+};
