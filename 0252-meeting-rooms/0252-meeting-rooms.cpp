9f0ef389421ac657cb046b974c9ce51c38956b40
AbdeliB <34876910+ShifaaAbdeli@users.noreply.github.com>
2024-03-14 16:47:33 -0700

    Time: 15 ms (33.41%), Space: 15 MB (46.98%) - LeetHub

---
+++ 0252-meeting-rooms/0252-meeting-rooms.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        
+        for (int i=0; i+1 < intervals.size(); ++i) {
+            if (intervals[i][1] > intervals[i+1][0]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
