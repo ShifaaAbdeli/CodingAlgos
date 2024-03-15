c2bbce4cd66e740a9aceecc650153ff1d1c603be
AbdeliB <34876910+ShifaaAbdeli@users.noreply.github.com>
2024-03-14 17:16:07 -0700

    Time: 8 ms (69.50%), Space: 4.1 MB (24.00%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.go
+++ 0252-meeting-rooms/0252-meeting-rooms.go
@@ -1,13 +1,19 @@
-class Solution {
-public:
-    bool canAttendMeetings(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end());
-        
-        for (int i=0; i+1 < intervals.size(); ++i) {
-            if (intervals[i][1] > intervals[i+1][0]) {
-                return false;
-            }
+func canAttendMeetings(intervals [][]int) bool {
+    // Sorting function for intervals
+    sort.Slice(intervals, func(i, j int) bool {
+        if intervals[i][0] == intervals[j][0] {
+            return intervals[i][1] < intervals[j][1]
+        } else {
+            return intervals[i][0] < intervals[j][0]
         }
-        return true;
+    })
+     
+    for i := 0; i + 1 < len(intervals); i++ {
+        if (intervals[i][1] > intervals[i+1][0]) {
+            return false
+        }
     }
-};
+    return true
+}
+
+// Time complexity: O(nlog(n)), space complexity: O(1)
