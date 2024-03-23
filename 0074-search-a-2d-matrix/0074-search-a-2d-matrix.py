025ac61a0a2fc4a5cc751ba457ce0a1d6cd72ef1
AbdeliB <34876910+ShifaaAbdeli@users.noreply.github.com>
2024-03-23 14:06:55 -0700

    Time: 42 ms (79.07%), Space: 17.3 MB (8.48%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,21 +1,43 @@
+"""
+1- Use binary search to find the ROW that may contain the tearget
+2- use the ROW found above and use binary search to find the COL that contain the target
+3- Time complexity is O(log(n)). Space is O(1)
+
+"""
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-
-        RAWs = len(matrix)
-        if RAWs == 0:
-            return False
-        COLs = len(matrix[0])
-    
-        l, r = 0, COLs * RAWs - 1
+        ROWS, COLS = len(matrix), len(matrix[0])
+        
+        top, bot = 0, ROWS - 1
+        while top <= bot:
+            row = (top + bot) // 2
+            if target < matrix[row][0]:
+                bot = row - 1
+            elif target > matrix[row][-1]:
+                top = row + 1
+            else:
+                break
+                
+   #     if not (top <= bot):
+  #          return False
+        
+        ## row where target is possibly located
+        row = (top + bot) // 2
+        ## search for possible colone for the tearget
+        l, r = 0, COLS-1
         while l <= r:
-            mid = l +((r - l)//2) # to avoid overflow issue
-            mid_element = matrix[mid//COLs][mid % COLs]
-            if target < mid_element:
+            mid = (l + r)//2
+            if target < matrix[row][mid]:
                 r = mid -1
-            elif target > mid_element:
+            elif target > matrix[row][mid]:
                 l = mid + 1
             else:
                 return True
         return False
-### Time complexity O(nlog(n)), Space O(1)
-    
+            
+        
+        
+        
+
+        
+        
