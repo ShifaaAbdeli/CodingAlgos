82cb2c6ac70c07c2d88cfed7f76db8ff5977086f
AbdeliB <34876910+ShifaaAbdeli@users.noreply.github.com>
2024-03-23 10:35:57 -0700

    Time: 54 ms (85.95%), Space: 17.7 MB (55.93%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,20 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {} # val: index
         
-        #mapList = {} # value, index
-        #for index, value in enumerate(nums):
-        #    diff = target - value
-        #    if diff in mapList:
-        #        return [mapList[diff], index]
-        #    mapList[value] = index
-        #return []
-
-
-        ###### Approach HashMap {nums[index]:index} #####
-        hashMapTwoSum = {}
-        for index, val in enumerate(nums):
-            diff = target - val
-            if diff in hashMapTwoSum:
-                return [index, hashMapTwoSum[diff]]
-            hashMapTwoSum[val] = index
-        
+        for idx, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [idx, prevMap[diff]]
+            prevMap[n] = idx
+            
