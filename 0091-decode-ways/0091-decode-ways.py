1eca2fb8d9c1745d854da78fb4a8722237c4a048
AbdeliB <34876910+ShifaaAbdeli@users.noreply.github.com>
2024-03-28 17:16:33 -0700

    Time: 43 ms (15.04%), Space: 16.5 MB (93.02%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -4,23 +4,27 @@ class Solution:
         dp = [0 for _ in range(len(s)+1)]
         
         # Initialize dp[0] to 1 and 
-        dp[0] = 1
+        #dp[0] = 1
+        twoBack = 1
         
         # dp[1] to 1 if fisrt char of s[0] is not '0'
-        dp[1] = 1 if s[0] != '0' else 0
+        #dp[1] = 1 if s[0] != '0' else 0
+        oneBack = 1 if s[0] != '0' else 0
         
-        for i in range(2, len(dp)):
-            
+        for i in range(1, len(s)):
+            current = 0
             # Check if single digit of s is possibly succesful decoding
-            if s[i-1] != '0':
-                dp[i] = dp[i-1] # s[i-1] is mapped to dp[i]
+            if s[i] != '0':
+                current = oneBack # s[i-1] is mapped to dp[i]
                 
             # Check if the two digit of s is possible succesful decoding
-            two_digit = int(s[i-2:i])
+            two_digit = int(s[i-1:i+1])
             if two_digit >= 10 and two_digit <= 26:
-                dp[i] += dp[i-2]
+                current += twoBack
+            twoBack = oneBack
+            oneBack = current
                 
         # The dp of the len(s) position and the number of possible decoding        
-        return dp[len(s)]
+        return oneBack
             
-            
+ ## Time complexity: O(n), Space : O(n)           
