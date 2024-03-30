5692c56e7637c49712033ce9f8b04a16d7964b32
AbdeliB <34876910+ShifaaAbdeli@users.noreply.github.com>
2024-03-30 10:54:28 -0700

    Time: 103 ms (11.71%), Space: 18.2 MB (80.70%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,37 +1,41 @@
+"""
+numCourses = []
+ai ---> bi, ci, di, ... ; preMap[ai] = [bi, ci, di, ...]
+
+- check special cases: numCources = []
+- build perMap of lists of all preRecusite of each crs 
+- check if cource visited
+- dfs traversal to check pre-recusite can be taking truly
+- check all cources schedule with dfs and return true if all completed or fasle
+
+"""
+
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        preMapAdjList = {i:[] for i in range(numCourses)}
-        visited = set()
-
-        for crs, prereq in prerequisites:
-            preMapAdjList[crs].append(prereq)
+        preMap = {i:[] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+        visitSet = set()
         
         def dfs(crs):
-            # Create hashMap for prerequite cources and
-            # visited set for adjList and mark the visited cours
-            if crs in visited: # Detecting a cycle
+            if crs in visitSet:
                 return False
-            if preMapAdjList[crs] == []:
-                return True
-
-            # Recursive dfs for each cours  
-            visited.add(crs)
-            for prereq in preMapAdjList[crs]:
-                if not dfs(prereq): return False
-
-            # Remove crs for visited set and 
-            # clear hashMap for crs
-            visited.remove(crs)
-            preMapAdjList[crs] = []
-            return True
-        # main call for dfs recursive
-        for crs in range(numCourses):
-            if not dfs(crs): return False
-        return True
-
-        #Time complexity O(m+n), space (m+n). 
-        # m is number of cources, n is the number of prerequisite
-
             
-
-         
+            if preMap[crs] == []:
+                return True
+            
+            visitSet.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+                
+            visitSet.remove(crs)
+            preMap[crs] = []
+            return True
+        
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return False
+        return True
+    
+## Time complexity: O(numCourses^2); Space: O(numCources)
